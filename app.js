e43f2c252826b6ab5a24bd77c76841502acc3c2f
Seth Leslie <sethleslie.au@gmail.com>
2021-05-30 20:54:51 +1000

    polling working but very jumping

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ import {
   import * as log from "https://deno.land/std/log/mod.ts";
   
   const client = new Client({
-    user: "assignment2",
+    user: "postgres",
     database: "poemZone",
     hostname: "localhost",
-    password: "Assignment2",
+    password: "2ur2l3Dov3",
     port: 5432,
   });
 
@@ -51,7 +51,6 @@ router.post("/api/login", async (context) => {
     context.response.status = 201;
     const db_user = results.rows[0];
     context.response.body = db_user;
-    console.log(password);
 
     // Check that the password matches
     const matches = sodium.crypto_pwhash_str_verify(
@@ -60,7 +59,6 @@ router.post("/api/login", async (context) => {
     );
 
     if (!matches) {
-      console.log("Incorrect password");
       context.response.status = 400;
       context.response.body = {"error": "Requires a name or a password"};
     return;
@@ -70,11 +68,41 @@ router.post("/api/login", async (context) => {
   }
 });
 
+router.post('/api/poems/:poem_id/posts', async (context) => {
+  if(!context.request.hasBody) {
+    context.response.status = 400;
+    context.response.body = {"error": "Expected a JSON object body"};
+    return;
+  }
+
+  const user = await context.request.body('json').value;
+
+  if(!user.text) {
+    context.response.status = 400;
+    context.response.body = {"error": "Requires a name or a description"};
+    return;
+  }
+
+  const insertResults = await client.queryObject`INSERT INTO POEMCOMMENTS 
+  (comment_text, user_id, poem_id) 
+  VALUES ( ${user.text}, ${user.user_id}, ${context.params.poem_id}) 
+  RETURNING (comment_id)`;
+
+  if(insertResults.rowCount === 0) {
+    context.response.status = 400;
+    context.response.body = {"error": "Error inserting comment"};
+    return;
+  }
+
+  context.response.status = 201;
+
+});
+
 /* Return the user with a given username */
 router.get('/api/users/:user_name', async (context) => {
   if (context.params.user_name) {
     const results = await client.queryObject`SELECT 
-    user_name, user_full_name 
+    user_id, user_name, user_full_name 
     FROM pzusers WHERE user_name=${context.params.user_name}`;
     if (!results.rows.length) {
       context.response.status = 400;
@@ -85,15 +113,36 @@ router.get('/api/users/:user_name', async (context) => {
   }
 });
 
+router.get('/api/poems/:poem_id/body', async (context) => {
+  if (context.params.poem_id) {
+    const results = await client.queryObject`SELECT 
+    poems.poem_id, poems.poem_body, poemcomments.comment_text, pzusers.user_name
+    FROM poems
+    INNER JOIN poemcomments ON poems.poem_id=poemcomments.poem_id
+    INNER JOIN pzusers ON poemcomments.user_id=pzusers.user_id
+    WHERE poems.poem_id = ${context.params.poem_id}
+    GROUP BY poemcomments.comment_id, poems.poem_id, poemcomments.comment_text, pzusers.user_name
+    ORDER BY poemcomments.comment_id`;
+
+    if (!results.rows.length) {
+      context.response.status = 400;
+      context.response.body = {"error": "Username not found"};
+    } else {
+      context.response.body = results.rows.map(r => (
+        {...r, _url: `/api/poems/${r.id}`}    // this is the "spread" operator
+      ))}
+}
+})
+
 // Set up a route to listen to /api/poems
 router.get("/api/poems", async (context) => {
-  const results = await client.queryObject
-    `SELECT poems.poem_id, poem_title, poem_body, poems.user_id, pzusers.user_name, CAST(AVG(ratings.poem_rating) AS DECIMAL(2,1)) AS avg_rating
-    FROM poems
-    INNER JOIN pzusers ON poems.user_id=pzusers.user_id
-    INNER JOIN ratings ON poems.poem_id=ratings.poem_id
-    GROUP BY poems.poem_id, pzusers.user_name
-    ORDER BY avg_rating DESC;`;
+  const results = await client.queryObject`SELECT 
+  poems.poem_id, poem_title, poem_body, poems.user_id, pzusers.user_name, CAST(AVG(ratings.poem_rating) AS DECIMAL(2,1)) AS avg_rating
+  FROM poems
+  INNER JOIN pzusers ON poems.user_id=pzusers.user_id
+  INNER JOIN ratings ON poems.poem_id=ratings.poem_id
+  GROUP BY poems.poem_id, pzusers.user_name
+  ORDER BY avg_rating DESC;`;
 
   context.response.body = results.rows.map(data => (
       {...data, _url: `/api/poems/${data.poem_id}`}    // this is the "spread" operator
--- static/js/frontend.js
+++ static/js/frontend.js
@@ -6,15 +6,14 @@ const app = document.getElementById("app");
 
 const start = () => {
     //If this is not a new session, login will be skipped
-    //if (sessionStorage.getItem('username') === null) {
+    if (sessionStorage.getItem('user_name') === null) {
       show(document.getElementById('login'));
-    //}
-   /*  else {
-      show(document.getElementById('topics'));
-      setGreeting(sessionStorage.getItem("name"));
-      getTopics();
-      functionsToPoll.push(getTopics);
-    } */
+    } else {
+      show(document.getElementById('poems'));
+      setGreeting(sessionStorage.getItem("user_full_name"));
+      getPoems();
+      functionsToPoll.push(getPoems);
+    }
   }
 
 function showError(error) {
@@ -69,19 +68,19 @@ const setGreeting = (fullName) => {
 };
 
 const loadPoems = (user_name) => {
-    console.log(`Now we can load all the poems for user: ${user_name}`);
     fetch(`${api}/users/${user_name}`)
       .then(response => response.json())
       .then(data => {
         sessionStorage.setItem('user_name', data.user_name);
         sessionStorage.setItem('user_full_name', data.user_full_name);
+        sessionStorage.setItem('user_id', data.user_id);
         //hide login and show poems
         hide(document.getElementById('login'));
         show(document.getElementById('poems'));
         setGreeting(data.user_full_name);
         getPoems();
-        //add getTopics to the list of functions to be called in the polling service
-        //functionsToPoll.push(getPoems);
+        //add getPoems to the list of functions to be called in the polling service
+        functionsToPoll.push(getPoems);
     })
     .catch((error) => {
         showError(error);
@@ -111,7 +110,7 @@ const getPoems = () => {
                 const poemItem = document.createElement('li');
                 poemItem.id = `poem-${poem.poem_id}`;
                 poemItem.className = 'poem-item';
-                poemItem.innerHTML = `<a onclick="getPostsForPoem(this)" data-id="${ poem.poem_id }">${ poem.poem_title } by ${ poem.user_name} AVG RATING: ${poem.avg_rating}</a>`;
+                poemItem.innerHTML = `<a onclick="getBodyForPoem(this)" data-id="${ poem.poem_id }">${ poem.poem_title } by ${ poem.user_name} AVG RATING: ${poem.avg_rating}</a>`;
                 /* if (poem.user_name === sessionStorage.getItem('user_name')) {
                     poemItem.innerHTML = `<button onclick="deletePoem(${poem.poem_id})" class="poem-delete-btn">Delete</button><a onclick="getPostsForPoem(this)" data-id="${ poem.poem_id }">${ poem.poem_title }</a>`;
                 } else {
@@ -125,5 +124,123 @@ const getPoems = () => {
       });
 };
 
+const getBodyForPoem = (element) => {
+    fetch(`${api}/poems/${ element.dataset.id }/body`)
+      .then(response => response.json())
+      .then((data) => {
+          let poemBody = document.getElementById(`poem-body-${ element.dataset.id }`);
+          let poemBodyCreated = false;
+          if (poemBody === null) {
+              poemBody = document.createElement('p');
+              poemBody.id = `poem-body-${ element.dataset.id }`;
+              poemBody.className = "poem-body";
+              poemBodyCreated = true;
+              poemBody.innerHTML = `<p>${ data[0].poem_body}</p>`
+          }
+
+          // Check if the post exists so that we don't duplicate it everytime we poll
+          let postList = document.getElementById(`post-list-${ element.dataset.id }`);
+          let postListCreated = false;
+
+          if (postList === null) {
+            postList = document.createElement('ul');
+            postList.id = `post-list-${ element.dataset.id }`;
+            // This is used to know if we ended up creating a new post list.
+            postListCreated = true;
+          };
+
+          const numberOfOldItems = postList.getElementsByTagName('li').length; 
+
+          //only add new items to the list using slice and number of old items
+          data.slice(numberOfOldItems).reduce(
+              (list, poem) => {
+                  const poemItem = document.createElement('li');
+                  poemItem.innerHTML = `<p>${ poem.comment_text} <br>--${poem.user_name}</p>`;
+                  list.appendChild(poemItem);
+                  return list;
+              },
+              postList
+          );
+
+          // If a new post list is created then add buttons and poll for new posts.
+          if (postListCreated) {
+            newPostInput = document.createElement('input');
+            //add id to input field so it only adds to relevant poem. This solved the problem
+            newPostInput.id = "post-input-" + element.dataset.id;
+            newPostButton = document.createElement('button');
+            newPostButton.innerHTML = 'Post';
+            //added a condition to check for input value
+            newPostButton.addEventListener('click', () => {
+              const myInput = document.getElementById("post-input-" + element.dataset.id);
+              if(myInput.value !== ""){
+                addPost(element.dataset.id, myInput)
+              }
+            },false
+            );
+
+            element.after(newPostButton);
+            element.after(newPostInput);
+            element.after(postList);
+            element.after(poemBody);
+
+            // Add this function with the current element to the list of functions to call during the poll.
+            //That way it only polls the topics that have been opened, which is cool.
+            const getBodyForPoemSpecific = () => getBodyForPoem(element);
+
+            //adding name of function to array.
+            functionsToPoll.push(getBodyForPoemSpecific);
+          };
+      });
+};
+
+const addPost = (id, newPostInput) => {
+    const newPost = {
+        user_name: sessionStorage.getItem('user_name'),
+        user_id: sessionStorage.getItem('user_id'),
+        text: newPostInput.value
+    };
+
+    // Optimistically try and add a new post
+    const postItem = document.createElement('li');
+    postItem.innerHTML = `<p>${newPostInput.value} <br>--${ sessionStorage.getItem('user_name') }</p>`;
+    document.getElementById(`post-list-${ id }`).appendChild(postItem);
+
+    fetch(`${api}/poems/${id}/posts`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPost)
+      })
+      .then((res) => {
+        if (res.status !== 201) {
+          throw `Couldn't insert post!`;
+        }
+        newPostInput.value = '';
+      })
+      .catch((error) => {
+        showError(error);
+        document.getElementById(`post-list-${ id }`).removeChild(postItem);
+      });
+};
+
+  //This method was shown to me by Asher Leslie
+  // This keeps track of which api endpoints have been used so that it only polls for data that
+  // the user has asked for.
+  const functionsToPoll = [];
+
+  const pollServer = (pollList) => {
+      setInterval(() => {
+          //taking the name in the array and calling it as a function by adding () to the name.
+          pollList.map((pollFunction) => pollFunction());
+        },
+        //set the delay between intervals in ms
+        3000
+      );
+  }
+
   // Initialise the application.
   start();
+
+  // Start the polling service.
+  pollServer(functionsToPoll);
